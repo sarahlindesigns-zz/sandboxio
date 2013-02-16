8345ba2d4dbbf26fcb887b1ac90827aee38809f7
mmansion <mmansion@kenzanmedia.com>
2013-02-16 11:28:13 -0500

    Adding local Angular resources

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -3,62 +3,112 @@ angular.module('myApp', ['carousel']);
 angular.module('carousel', []).
 
 directive('carousel', function() { return {
-   restrict: 'E',
-   scope: true,
-   transclude: true,
-   replace: false,
-   controller: 'CarouselCtrl',
-   templateUrl: 'Carousel.html',
-   link: function postLink(scope, elem, attrs, ctrl) {
+
+  restrict    :   'E',
+  scope       :   true,
+  transclude  :   true,
+  replace     :   false,
+  controller  :   'CarouselCtrl',
+  templateUrl :   'Carousel.html',
+
+  link: function(scope, elem, attrs, ctrl) {
+
+    /**
+      *
+      * UI Behavior
+      *
+      */
      
-     var self = scope;
+    var self = scope;
 
-     //elements
-     var carousel = $(elem).find('.carousel');
-     var tiles    = $(elem).find('.tile');
+    //elements
+    var carousel = $(elem).find('.carousel');
+    var tiles    = $(elem).find('.tile');
+    var nxtBtn   = $(elem).find('.carousel-next');
+    var prvBtn   = $(elem).find('.carousel-prev');
 
-     var carouselWidth  = carousel.width();
-     var tileWidth  = tiles.width();
-     var tileOffset = tiles.outerWidth();
+    //measurements
+    var carouselWidth  = carousel.width();
+    var tileWidth      = tiles.width();
+    var tileOffset     = tiles.outerWidth();
+    var elementsPerRow = carouselWidth/tileOffset;
 
+    //flags
+    var showingFirstSet = true;
 
-     var elementsPerRow = carouselWidth/tileOffset;
+    self.addEventHandlers = function() {
+  
+      $(window).resize(function() {
+        //updateElement PerRow();
+      });
 
-     console.log(elementsPerRow);
-    
+      $(elem).mouseover(function() {
+        showUI();
+      });
+       
+      $(elem).mouseout(function() {
+        hideUI();
+      });
 
-     $(window).resize(function() {
-      updateElementPerRow();
-     });
+      prvBtn.click(function() {
+        console.log("load prev");
+        if(ctrl.getPageNo())
+        ctrl.getPrev();
+        showingFirstSet = (ctrl.getPageNo() < 2);
+        showUI();
+      });
 
-     $(elem).mouseover(function() {
-       if(!ctrl.isMobile()) {
-         $('.carousel').addClass('show-ui');
-       }
-     });
-     
-     $(elem).mouseout(function() {
-       if(!ctrl.isMobile()) {
-         $('.carousel').removeClass('show-ui');
-       }
-     });
+      nxtBtn.click(function() {
+        console.log("load next");
+        ctrl.getNext();
+        showingFirstSet = (ctrl.getPageNo() < 2);
+        showUI();
+      });
+    };
 
-     $('.carousel-prev').click(function() {
-       console.log("load prev");
-     });
+    //helper functions
+    function getDisplayWidth() {
+      //return carouselWidth
+    }
 
-     $('.carousel-next').click(function() {
-       console.log("load next");
-       getMaskWidth();
-     });
+    function showUI() {
+      if(!ctrl.isMobile()) {
+        if(showingFirstSet) {
+         carousel.removeClass('show-prev-ui');
+         carousel.addClass('show-next-ui');
+        } else {
+         carousel.addClass('show-prev-ui');
+         carousel.addClass('show-next-ui');
+        }  
+      }
+    }
 
+    function hideUI() {
+      if(!ctrl.isMobile()) {
+        carousel.removeClass('show-prev-ui');
+        carousel.removeClass('show-next-ui');
+      }
+    }
 
-   }
+    function adjustIndent() { //adjust left offset on first row of results
+
+    }
+
+    self.addEventHandlers();   
+  }
 };}).
 
 controller('CarouselCtrl', ['$scope', '$http', function($scope, $http) {
 
+  /**
+    *
+    * UI Logic
+    *
+    */
+
   var self = $scope;
+
+  var pageNo = 1; //current display results set
   
   //PUBLIC CONTROLLER METHODS
   
@@ -70,6 +120,17 @@ controller('CarouselCtrl', ['$scope', '$http', function($scope, $http) {
      }
   }; 
 
-  
+  self.getNext = function() {
+    pageNo++;
+  };
+
+  self.getPrev = function() {
+    pageNo--;
+  };
+
+  self.getPageNo = function() {
+    return pageNo;
+  };
+
   return self;
 }]);
