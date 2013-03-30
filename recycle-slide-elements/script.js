00c80d1a96f5d5a036cbc22cdef0308810809704
mmansion <mikhail@mansion.fm>
2013-03-30 18:47:02 -0400

    Adding ability to update data and populate new dom element sequences on scroll

--- recycle-slide-elements/script.js
+++ recycle-slide-elements/script.js
@@ -14,11 +14,8 @@ $(document).ready(function() {
   var spacing   = 20;
   var itemWidth = 140;
   
-  /*  [][][], [][][] <-Offleft | [inview] [inview] [inview]  | Offright-> [][][], [][][]  */
 
-  var multiple  = 5; 
-
-  var results = new Array(10); //XXX: for testing
+  var results = new Array(13); //XXX: for testing
   var img_path = '../img/';   //XXX: for testing
 
   var currentSet = 1;
@@ -51,7 +48,23 @@ $(document).ready(function() {
     console.log('items in view => ' + noItemsInView);
 
     itemViewWidth = noItemsInView*(itemWidth+spacing)-spacing;
-    totalNoElements = noItemsInView * multiple;
+
+    /*
+
+    q. why a multiple of 5?
+    
+    a. you must visualize the carousel; it has a finite amount of elements to recycle
+
+      here is a crude representation:
+
+      <- [][][], [][][] | [inview] [inview] [inview] | [][][], [][][] ->
+
+      note: while there are 5 sets of groups, only 1 is visible at a time
+            the additional 2 sets on each end account for an innevitable 
+            overlap of elements that peek into the current view, by design
+    */
+
+    totalNoElements = noItemsInView * 5;
 
     firstInViewIx = noItemsInView*2;
 
@@ -65,13 +78,13 @@ $(document).ready(function() {
     totalItemsInRange = items.length;
     pixelSlideAmount  = items.outerWidth(true)*noItemsInView;
 
-    totalSets = Math.ceil(results.length / noItemsInView);
+    totalSets = Math.ceil(results.length / noItemsInView); //determine how many view sets (pagination)
 
     $('.output').html(currentSet + ' of ' + totalSets); //XXX: print out the set info
 
     slide.css('margin-left', -pixelSlideAmount*2); //set the intial slide position
 
-    //updateCarouselData();
+    updateCarouselData();
   }
 
   function goSlide(dir) {//slide the carousel by direction
@@ -84,7 +97,7 @@ $(document).ready(function() {
 
         'margin-left': (dir) ? '-=' + pixelSlideAmount: '+=' + pixelSlideAmount
 
-      }, 500, 'linear', function() { //when complete, update the dom/data, and make any necessary adjustments
+      }, 350, 'linear', function() { //when complete, update the dom/data, and make any necessary adjustments
 
         //get range of elements to chop off and move
         var range = (dir) ? items.slice(0, noItemsInView): items.slice(items.length-noItemsInView, items.length);
@@ -115,18 +128,40 @@ $(document).ready(function() {
         $('.output').html(currentSet + ' of ' + totalSets);
 
         //finally, we update the displayed data within our item elements
-        //updateCarouselData(dir);
+        updateCarouselData(dir);
 
       });
     }
   }
 
   function updateCarouselData(dir) {
-    if(typeof(dir) !== 'undefined') {
-      console.log(dir);
-    } else {
-      for(var j = 0; j < items.length; j++) {
-        $(items[j + firstInViewIx]).find('img').attr('src', results[j]);
+    var dataIx;
+    //after each scroll event, we'll update the data within the new item sequence
+    if(typeof(dir) !== 'undefined') {//if no direction is provided, we initialize
+      if(dir) { //if scrolling forward, update the elements at the end
+        dataIx = (noItemsInView*2)+(noItemsInView*(currentSet-1));
+        for(var i = 0; i < noItemsInView; i++) {
+          //TODO: create a separate function for populating the elements
+          if((dataIx+i)<results.length) {//if within range
+            $(items[items.length-noItemsInView+i]).find('img').attr('src', results[dataIx+i]);
+          } else {
+            $(items[items.length-noItemsInView+i]).find('img').attr('src', '');
+          }
+        }
+      } else { //if scrolling back, update the elements at the beginning
+        dataIx = (noItemsInView*currentSet)-(noItemsInView*3);
+        for(var ii = 0; ii < noItemsInView; ii++) {
+          //TODO: create a separate function for populating the elements
+          if(dataIx >= 0) {
+            $(items[ii]).find('img').attr('src', results[dataIx+ii]);
+          } else {
+            $(items[ii]).find('img').attr('src', '');
+          }
+        }
+      }
+    } else { //no direction argument provided, we will initialize the data
+      for(var iii = 0; iii < items.length; iii++) {
+        $(items[iii + firstInViewIx]).find('img').attr('src', results[iii]);i
       }
     }
   }
